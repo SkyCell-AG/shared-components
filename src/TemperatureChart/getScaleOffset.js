a0aa242a9894c4fd6efb4abb4dd96e20f5e88643
Dmytro <portyanka.d@gmail.com>
2021-02-22 18:48:58 +0100

    TemperatureChartk

---
+++ src/TemperatureChart/getScaleOffset.js
@@ -0,0 +1,14 @@
+const getScaleOffset = ({
+  ambient,
+  simulated
+}) => {
+  const max = Math.max(...ambient, ...simulated)
+  const min = Math.min(...ambient, ...simulated)
+
+  const range = max - min
+  const scale = range / 100
+
+  return scale
+}
+
+export default getScaleOffset
---
+++ src/stories/TemperatureChart.stories.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import TemperatureChart from 'TemperatureChart'
+
+const TemperatureChartStory = {
+  title: 'Example/TemperatureChart',
+  component: TemperatureChart,
+  argTypes: {
+    ambient: {
+      control: 'array',
+    },
+    simulated: {
+      control: 'array',
+    },
+  },
+};
+
+const Template = ({
+  ambient,
+  simulated,
+  ...rest
+}) => {
+  return (
+    <TemperatureChart
+      simulated={simulated.map((num) => parseInt(num, 10))}
+      ambient={ambient.map((num) => parseInt(num, 10))}
+      {...rest}
+    />
+  )
+}
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  ambient: [20,52,32,40,58,22,52,32,41,54,20,52,32,40,58,22,52,32,41,54],
+  simulated: [2,3,1,4,2,2,3,1,4,2,2,3,1,4,2,2,3,1,4,2],
+  energyLevel: [100, 98, 96, 97, 95, 100, 98, 96, 97, 95,100, 98, 96, 97, 95, 100, 98, 96, 97, 95],
+  excursion: 4,
+};
+
+export default TemperatureChartStory
