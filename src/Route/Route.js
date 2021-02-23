e19c39758b2ab5bc85648a602887d3e6f7da7409
Dmytro <portyanka.d@gmail.com>
2021-02-23 19:12:59 +0100

    added Route component

---
+++ src/Route/Route.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  Graticule,
+  Marker,
+  Line,
+} from 'react-simple-maps'
+
+const geoUrl = 'https://prodskynetstaticwebsite.blob.core.windows.net/shared/world-110m.json'
+
+const propTypes = {
+  destinations: PropTypes.arrayOf(PropTypes.number).isRequired,
+}
+
+const Route = ({
+  destinations,
+}) => {
+  console.log(destinations)
+
+  const first = destinations[0]
+  const last = destinations[destinations.length - 1]
+
+  return (
+    <ComposableMap
+      projection="geoEqualEarth"
+    >
+      <Graticule stroke="#DDD" />
+      <Geographies
+        geography={geoUrl}
+        fill="#D6D6DA"
+        stroke="#FFFFFF"
+        strokeWidth={0.5}
+      >
+        {({ geographies }) =>
+            geographies.map(geo => <Geography key={geo.rsmKey} geography={geo} />)
+        }
+      </Geographies>
+      <Line
+        coordinates={destinations}
+        stroke="#F53"
+        strokeWidth={2}
+      />
+      <Marker coordinates={first}>
+        <circle r={4} fill="#F53" />
+      </Marker>
+      <Marker coordinates={last}>
+        <circle r={4} fill="#A43" />
+      </Marker>
+    </ComposableMap>
+  )
+}
+
+Route.propTypes = propTypes
+
+export default Route
---
+++ src/stories/Route.stories.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import Route from 'Route'
+
+const RouteStory = {
+  title: 'Example/Route',
+  component: Route,
+  argTypes: {
+    destinations: {
+      control: 'array',
+      description: 'Array of tuples [longitude, latitude]'
+    },
+  },
+};
+
+const Template = (props) => {
+  return (
+    <Route {...props} />
+  )
+}
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  destinations: [
+    [-73.780968, 40.641766],
+    [-112.937469, 40.199387],
+    [55.364445, 25.252777],
+  ],
+};
+
+export default RouteStory
