861964d01cd266cf3319b5054a0186a4aceceb5b
Anastasiia Manilnikova <mailto:Anastasiia.Manilnikova@skycell.ch>
2021-03-21 17:56:11 +0200

    add Card component

---
+++ src/stories/Card.stories.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import Card from 'Card'
+
+const CardStory = {
+    title: 'Example/Card',
+    component: Card,
+    argTypes: {
+        title: {
+            control: 'text',
+        },
+        children: {
+            control: 'text'
+        },
+    },
+};
+
+const Template = (props) => {
+    return (
+        <Card {...props} />
+    )
+}
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+    title: 'Title Card',
+    children: 'body',
+};
+
+export default CardStory
