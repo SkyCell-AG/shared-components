badcf1d144887ec9c2ef85a684069d22c8c71b8d
taraspaliy <73470053+taraspaliy2020@users.noreply.github.com>
2021-07-02 12:03:03 +0300

    ID-3569-add-stories

--- src/stories/hooks.stories.js
+++ src/stories/hooks.stories.js
@@ -2,13 +2,14 @@ import React from 'react'
 import {
     QueryClient,
     QueryClientProvider,
-  } from 'react-query'
-  import Auth from '@skycell-ag/auth'
+} from 'react-query'
+import Auth from '@skycell-ag/auth'
 import {
     useUserContacts,
     useUserAddresses,
     useProcesses,
     useUserLocations,
+    useEmptyProcess,
 } from 'hooks/businessObjects'
 import ErrorBoundary from 'ErrorBoundary'
 import { initVariables } from 'init'
@@ -16,21 +17,21 @@ import { initVariables } from 'init'
 export default {
     decorators: [(Story) => {
         const queryClient = new QueryClient()
-        initVariables({'REACT_APP_SKYMIND_API': 'https://skymind.dev.skycell.ch'})
+        initVariables({ 'REACT_APP_SKYMIND_API': 'https://skymind.dev.skycell.ch' })
         return (
             <ErrorBoundary>
                 <Auth>
-                    <QueryClientProvider client={queryClient}>   
+                    <QueryClientProvider client={queryClient}>
                         <Story />
-                    </QueryClientProvider>          
+                    </QueryClientProvider>
                 </Auth>
-            </ErrorBoundary>               
+            </ErrorBoundary>
         )
     }],
     title: 'Hooks',
 }
 
-const ComponentWithHook = (props) => {     
+const ComponentWithHook = (props) => {
     const {
         data,
         isLoading,
@@ -41,63 +42,77 @@ const ComponentWithHook = (props) => {
         return (<div>Loading</div>)
     }
     if (error) {
-        throw error       
+        throw error
     }
-    
-    const formatedData = props.formater ? props.formater(data): data
-    
+
+    const formatedData = props.formater ? props.formater(data) : data
+
     return (
         <div>
-           <div>{`${props.text}: ${formatedData}`}</div>           
+            <div>{`${props.text}: ${formatedData}`}</div>
         </div>
     )
 }
 
-export const UserContacts= () => {
+export const UserContacts = () => {
     return (
         <ComponentWithHook
             text="Current user contact"
             hook={useUserContacts}
-            formater={(contacts)=>{
-                return contacts && contacts.length ? JSON.stringify(contacts): ''
+            formater={(contacts) => {
+                return contacts && contacts.length ? JSON.stringify(contacts) : ''
             }}
-        />  
+        />
     )
- }
+}
 
- export const UserAddresses= () => {
+export const UserAddresses = () => {
     return (
-        <ComponentWithHook 
+        <ComponentWithHook
             text="Current user adresses"
             hook={useUserAddresses}
-            formater={(addresses)=>{               
-                return addresses && addresses.length ? addresses.map((address)=> address.addressName).join(', '): ''
-            }}            
-        />  
+            formater={(addresses) => {
+                return addresses && addresses.length ? addresses.map((address) => address.addressName).join(', ') : ''
+            }}
+        />
     )
- }
- export const UserLocations= () => {
+}
+export const UserLocations = () => {
     return (
-        <ComponentWithHook 
+        <ComponentWithHook
             text="Current user locations"
             hook={useUserLocations}
-            formater={(locations)=>{
-                return locations && locations.length ? locations.map((location)=> location.locationName).join(', '): ''
-            }}            
-        />  
+            formater={(locations) => {
+                return locations && locations.length ? locations.map((location) => location.locationName).join(', ') : ''
+            }}
+        />
     )
- }
+}
 
- export const LocalesProcesses= () => {
+export const LocalesProcesses = () => {
     return (
-        <ComponentWithHook 
+        <ComponentWithHook
             text="Available Processes's names for Location with id=1"
             hook={useProcesses}
             hookParams={1}
-            formater={(processes)=>{               
-                return processes && processes.length ? processes.map((process)=> process.processLabel).join(', '): ''
-            }}            
-        />  
+            formater={(processes) => {
+                return processes && processes.length ? processes.map((process) => process.processLabel).join(', ') : ''
+            }}
+        />
     )
- }
+}
+
+export const EmptyProcess = () => {
+    return (
+        <ComponentWithHook
+            text="Empty Process for SENDING"
+            hook={useEmptyProcess}
+            hookParams={'SENDING'}
+            formater={(process) => {
+                return process ? process.processLabel : ''
+            }}
+        />
+    )
+}
+
 
