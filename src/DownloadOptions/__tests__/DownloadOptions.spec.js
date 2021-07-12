f9edb0ae86a4b30e6f949d398d29deb93675a094
Nuria <nuria@Nurias-MacBook-Pro.local>
2021-07-12 15:46:55 +0100

    add download test

--- src/DownloadOptions/__tests__/DownloadOptions.spec.js
+++ src/DownloadOptions/__tests__/DownloadOptions.spec.js
@@ -3,7 +3,11 @@ import {
     shallow,
 } from 'enzyme'
 
+import downloadDocument from '../../utils/downloadDocument'
+
 import DownloadOptions from '../DownloadOptions'
+
+jest.mock('../../utils/downloadDocument')
 
 describe('shared-components/DownloadOptions', () => {
     it('should match snapshot', () => {
@@ -29,6 +33,40 @@ describe('shared-components/DownloadOptions', () => {
         )
 
         expect(wrapper).toMatchSnapshot()
+    })
+
+    it('Document Print is loaded', (done) => {
+        const wrapper = shallow(
+            <DownloadOptions
+                showCsvButton
+                showTempRange
+                serialNumber="031-10279"
+                sensorData={[[
+                    new Date('August 16, 2018 06:50:00'),
+                    24.7,
+                    null,
+                    null,
+                    21.5,
+                    null,
+                    null,
+                ]]}
+                onCheckShowTempRange={jest.fn()}
+            />,
+        )
+
+        wrapper.find({
+            label: 'csv',
+        }).prop('onClick')()
+
+        setTimeout(() => {
+            expect(downloadDocument).toHaveBeenCalledWith({
+                headers: {
+                    'content-type': 'text/csv',
+                },
+                data: 'SERIAL_NUMBER,TIMESTAMP,AMBIENT_TEMPERATURE,INTERNAL_TEMPERATURE\n031-10279,16.08.2018 06:50,24.7,21.5',
+            }, 'sensor_data_031-10279')
+            done()
+        })
     })
 
     it('should generate csv data', () => {
