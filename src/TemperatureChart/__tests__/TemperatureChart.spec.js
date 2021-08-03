895ebba0b3f159a907398b9fe3376aa2a44b5c1a
Nuria <nuria@Nurias-MacBook-Pro.local>
2021-08-03 10:30:54 +0100

    fix test

--- src/TemperatureChart/__tests__/TemperatureChart.spec.js
+++ src/TemperatureChart/__tests__/TemperatureChart.spec.js
@@ -5,15 +5,15 @@ import {
 
 import TemperatureChart from '../TemperatureChart'
 
-jest.mock('victory', () => {
-    return {
-        VictoryLine: () => { return <div>VictoryLine</div> },
-        VictoryChart: () => { return <div>VictoryChart</div> },
-        VictoryAxis: () => { return <div>VictoryAxis</div> },
-        LineSegment: () => { return <div>LineSegment</div> },
-        VictoryLabel: () => { return <div>VictoryLabel</div> },
-    }
-})
+// jest.mock('victory', () => {
+//     return {
+//         VictoryLine: () => { return <div>VictoryLine</div> },
+//         VictoryChart: () => { return <div>VictoryChart</div> },
+//         VictoryAxis: () => { return <div>VictoryAxis</div> },
+//         LineSegment: () => { return <div>LineSegment</div> },
+//         VictoryLabel: () => { return <div>VictoryLabel</div> },
+//     }
+// })
 
 describe('TemperatureChart', () => {
     it('TemperatureChart success', () => {
