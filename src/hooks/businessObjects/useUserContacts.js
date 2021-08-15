b49c1766e734a98d55c10d93207fbf3cb1a50425
Dmytro <portyanka.d@gmail.com>
2021-08-15 16:30:41 +0200

    removed auth from dependencies

--- src/hooks/businessObjects/useUserContacts.js
+++
@@ -1,8 +0,0 @@
-import getContacts from './getContacts'
-import useQueryGetUserContacts from './useQueryGetUserContacts'
-
-const useUserContacts = () => {
-    return useQueryGetUserContacts(getContacts)
-}
-
-export default useUserContacts
