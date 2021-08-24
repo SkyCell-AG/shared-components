0afc3d752040156de6acb0abb8b070dbfb281f38
Dmytro <portyanka.d@gmail.com>
2021-08-24 16:40:08 +0300

    cover no fullscreen

--- src/TemperatureChart2/__tests__/TemperatureChart2.spec.js
+++ src/TemperatureChart2/__tests__/TemperatureChart2.spec.js
@@ -3,6 +3,7 @@ import {
     shallow,
 } from 'enzyme'
 
+import FullScreen from 'FullScreen'
 import TemperatureChart2 from '../TemperatureChart2'
 
 it('TemperatureChart success', () => {
@@ -34,5 +35,41 @@ it('TemperatureChart success', () => {
         />,
     )
 
+    expect(wrapper.find(FullScreen).length).toEqual(1)
     expect(wrapper).toMatchSnapshot()
+})
+
+it('TemperatureChart no fullscreen button', () => {
+    const wrapper = shallow(
+        <TemperatureChart2
+            allowFullScreen={false}
+            sensorData={[
+                {
+                    d: [
+                        18.1,
+                        5.3,
+                    ],
+                    t: '29.03.2021 10:40',
+                },
+                {
+                    d: [
+                        18.1,
+                        5.3,
+                    ],
+                    t: '29.03.2021 10:50',
+                },
+                {
+                    d: [
+                        18.2,
+                        5.3,
+                    ],
+                    t: '29.03.2021 11:00',
+                },
+            ]}
+        />,
+    )
+
+    expect(
+        wrapper.find(FullScreen).length,
+    ).toEqual(0)
 })
