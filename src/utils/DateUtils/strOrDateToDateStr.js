76e7e793d220e216397537e78ee001288bd1abf1
jmiklevska <85491387+jmiklevska@users.noreply.github.com>
2022-06-07 17:31:10 +0300

    [ID-7775] fix date string convert issue in DateRangeSelector

---
+++ src/utils/DateUtils/strOrDateToDateStr.js
@@ -0,0 +1,11 @@
+import dateToDateStr from './dateToDateStr'
+import strToDate from './strToDate'
+
+const strOrDateToDateStr = (value) => {
+    if (typeof value === 'string') {
+        return dateToDateStr(strToDate(value))
+    }
+    return dateToDateStr(value)
+}
+
+export default strOrDateToDateStr
